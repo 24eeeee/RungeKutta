3f42ea94f8b1e25fbdc98591c30673f8d46e0915
e24 <smyshlyaev1104@yandex.ru>
2023-06-30 00:44:22 +0300

    add plot

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from prettytable import PrettyTable
+import numpy as np
+import matplotlib.pyplot as plt
 
 
 def frange(start, stop=None, step=None):
@@ -24,8 +25,6 @@ def read_data_from_standard_input():
     print("Input k:")
     k = float(input())
 
-    print("Input x_0:")
-    x_0 = float(input())
     print("Input y_0:")
     y_0 = float(input())
     print("Input dy_0:")
@@ -36,38 +35,43 @@ def read_data_from_standard_input():
     print("Input x_final:")
     x_n = float(input())
 
-    return m, p, k, x_0, y_0, dy_0, h, x_n
+    return m, p, k, y_0, dy_0, h, x_n
 
 
 def runge_kuttas(m: float, p: float, k: float, x_0: float, y_0: float, dy_0: float, h: float, x_n: float):
     ddy = lambda m, p, k, dy, y: (-p * dy - k * y) / m
     nxt = lambda k1, k2, k3, k4: (k1 + 2 * k2 + 2 * k3 + k4) / 6
 
-    result = [(x_0, y_0, dy_0)]
+    result_x = [x_0]
+    result_y = [y_0]
+    result_dy = [dy_0]
 
     for iter_x in frange(x_0, x_n, h):
-        k1 = h * result[-1][2]
-        dk1 = h * ddy(m, p, k, result[-1][2], result[-1][1])
+        k1 = h * result_dy[-1]
+        dk1 = h * ddy(m, p, k, result_dy[-1], result_y[-1])
 
-        k2 = h * (result[-1][2] + dk1 / 2)
-        dk2 = h * ddy(m, p, k, result[-1][2] + dk1 / 2, result[-1][1] + k1 / 2)
+        k2 = h * (result_dy[-1] + dk1 / 2)
+        dk2 = h * ddy(m, p, k, result_dy[-1] + dk1 / 2, result_y[-1] + k1 / 2)
 
-        k3 = h * (result[-1][2] + dk2 / 2)
-        dk3 = h * ddy(m, p, k, result[-1][2] + dk2 / 2, result[-1][1] + k2 / 2)
+        k3 = h * (result_dy[-1] + dk2 / 2)
+        dk3 = h * ddy(m, p, k, result_dy[-1] + dk2 / 2, result_y[-1] + k2 / 2)
 
-        k4 = h * (result[-1][2] + dk3)
-        dk4 = h * ddy(m, p, k, result[-1][2] + dk3, result[-1][1] + k3)
+        k4 = h * (result_dy[-1] + dk3)
+        dk4 = h * ddy(m, p, k, result_dy[-1] + dk3, result_y[-1] + k3)
 
-        result.append((round(iter_x + h, len(str(h)) - 1), result[-1][1] + nxt(k1, k2, k3, k4), result[-1][2] + nxt(dk1, dk2, dk3, dk4)))
+        result_x.append(round(iter_x + h, len(str(h)) - 1))
+        result_y.append(result_y[-1] + nxt(k1, k2, k3, k4))
+        result_dy.append(result_dy[-1] + nxt(dk1, dk2, dk3, dk4))
 
-    return result
-    pass
+    return result_x, result_y, result_dy
 
 
-m, p, k, x_0, y_0, dy_0, h, x_n = read_data_from_standard_input()
-res = runge_kuttas(m, p, k, x_0, y_0, dy_0, h, x_n)
-header = ['x', 'y', 'dy']
-table = PrettyTable(header)
-for i in res:
-    table.add_row(i)
-print(table)
+m, p, k, y_0, dy_0, h, x_n = read_data_from_standard_input()
+x_0 = 0
+res_x, res_y, res_dy = runge_kuttas(m, p, k, x_0, y_0, dy_0, h, x_n)
+x = np.array(res_x)
+y = np.array(res_y)
+plt.plot(res_x, res_y, label='y')
+plt.plot(res_x, res_dy, label='dy')
+plt.legend(loc='best')
+plt.show()
